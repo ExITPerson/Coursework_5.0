73470f3e4d88bec0815c1bf63238b9841ba96611
Ex_person <baranovs11@yandex.ru>
2025-10-23 23:15:37 +0300

    User view set

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'habits',
 
     'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,17 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import UserViewSet
+
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+router = DefaultRouter()
+router.register(r'user', UserViewSet, basename='user')
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls
